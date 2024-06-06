5e89f0de6687579a1d5a39ea7f90d72b5fc7c9bb
Pankhuri-75 <84667167+Pankhuri-75@users.noreply.github.com>
2024-06-06 10:58:15 +0530

    Updated backend URL

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 
 import "./CartPage.css";
+import config from "../../config.json";
 import UserContext from "../../contexts/UserContext";
 import CartContext from "../../contexts/CartContext";
 import remove from "../../assets/remove.png";
@@ -41,7 +42,7 @@ const CartPage = () => {
     <section className="align_center cart_page">
       <div className="align_center user_info">
         <img
-          src={`http://localhost:5000/profile/${user?.profilePic}`}
+          src={`${config.backendURL}/profile/${user?.profilePic}`}
           alt="user profile"
         />
         <div>
